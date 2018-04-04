1092bfc359a7a2d211ab2213c045afae553bcbf3
teddy <teddy@sfc.wide.ad.jp>
2018-04-04 22:23:23 +0900

    make it more readable

--- systemd/systemd.go
+++ systemd/systemd.go
@@ -8,8 +8,16 @@ import (
 	"github.com/godbus/dbus"
 )
 
+const destBus = "org.freedesktop.systemd1"
+const objectPath = "/org/freedesktop/systemd1"
+const getMethod = "org.freedesktop.DBus.Properties.Get"
+const mngerMethod = "org.freedesktop.systemd1.Manager"
+const destUnit = "org.freedesktop.systemd1.Unit"
+const destService = "org.freedesktop.systemd1.Service"
+
 // Get ...
 func Get(ID string) *model.Unit {
+	var dstService = ID + ".service"
 
 	conn, err := dbus.SystemBus()
 	if err != nil {
@@ -18,42 +26,42 @@ func Get(ID string) *model.Unit {
 	}
 
 	var path dbus.ObjectPath
-	err = conn.Object("org.freedesktop.systemd1", "/org/freedesktop/systemd1").Call("org.freedesktop.systemd1.Manager.GetUnit", 0, ID+".service").Store(&path)
+	err = conn.Object(destBus, objectPath).Call(mngerMethod+".GetUnit", 0, dstService).Store(&path)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to get object path:", err)
 		return nil
 	}
 
 	var desc string
-	err = conn.Object("org.freedesktop.systemd1", path).Call("org.freedesktop.DBus.Properties.Get", 0, "org.freedesktop.systemd1.Unit", "Description").Store(&desc)
+	err = conn.Object(destBus, path).Call(getMethod, 0, destUnit, "Description").Store(&desc)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to get description:", err)
 		return nil
 	}
 
 	var loadStatus string
-	err = conn.Object("org.freedesktop.systemd1", path).Call("org.freedesktop.DBus.Properties.Get", 0, "org.freedesktop.systemd1.Unit", "LoadState").Store(&loadStatus)
+	err = conn.Object(destBus, path).Call(getMethod, 0, destUnit, "LoadState").Store(&loadStatus)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to get load status:", err)
 		return nil
 	}
 
 	var activeStatus string
-	err = conn.Object("org.freedesktop.systemd1", path).Call("org.freedesktop.DBus.Properties.Get", 0, "org.freedesktop.systemd1.Unit", "ActiveState").Store(&activeStatus)
+	err = conn.Object(destBus, path).Call(getMethod, 0, destUnit, "ActiveState").Store(&activeStatus)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to get active status:", err)
 		return nil
 	}
 
 	var unitFileState string
-	err = conn.Object("org.freedesktop.systemd1", path).Call("org.freedesktop.DBus.Properties.Get", 0, "org.freedesktop.systemd1.Unit", "UnitFileState").Store(&unitFileState)
+	err = conn.Object(destBus, path).Call(getMethod, 0, destUnit, "UnitFileState").Store(&unitFileState)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to get unit file status:", err)
 		return nil
 	}
 
 	var pid int
-	err = conn.Object("org.freedesktop.systemd1", path).Call("org.freedesktop.DBus.Properties.Get", 0, "org.freedesktop.systemd1.Service", "MainPID").Store(&pid)
+	err = conn.Object(destBus, path).Call(getMethod, 0, destService, "MainPID").Store(&pid)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to get main pid:", err)
 		return nil
@@ -73,6 +81,8 @@ func Get(ID string) *model.Unit {
 
 // Post ...
 func Post(ID string, action string, mode string) error {
+	var dstService = ID + ".service"
+
 	conn, err := dbus.SystemBus()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to connect to system bus:", err)
@@ -81,17 +91,17 @@ func Post(ID string, action string, mode string) error {
 
 	var path dbus.ObjectPath
 
-	obj := conn.Object("org.freedesktop.systemd1", "/org/freedesktop/systemd1")
+	obj := conn.Object(destBus, objectPath)
 
 	switch action {
 	case "start":
-		err = obj.Call("org.freedesktop.systemd1.Manager.StartUnit", 0, ID+".service", mode).Store(&path)
+		err = obj.Call(mngerMethod+".StartUnit", 0, dstService, mode).Store(&path)
 	case "restart":
-		err = obj.Call("org.freedesktop.systemd1.Manager.ReStartUnit", 0, ID+".service", mode).Store(&path)
+		err = obj.Call(mngerMethod+".ReStartUnit", 0, dstService, mode).Store(&path)
 	case "stop":
-		err = obj.Call("org.freedesktop.systemd1.Manager.StopUnit", 0, ID+".service", mode).Store(&path)
+		err = obj.Call(mngerMethod+".StopUnit", 0, dstService, mode).Store(&path)
 	case "reload":
-		err = obj.Call("org.freedesktop.systemd1.Manager.ReloadUnit", 0, ID+".service", mode).Store(&path)
+		err = obj.Call(mngerMethod+".ReloadUnit", 0, dstService, mode).Store(&path)
 	default:
 		fmt.Fprintln(os.Stderr, "Unknown action:", err)
 		return nil
