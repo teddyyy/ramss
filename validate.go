d06c7b8a2d1126e948e79a0aa8bf2cbe9bf212e5
teddy <teddy@sfc.wide.ad.jp>
2019-02-01 21:22:14 +0900

    add lint

--- validate.go
+++ validate.go
@@ -2,10 +2,12 @@ package main
 
 import "github.com/go-playground/validator"
 
+// Validator ...
 type Validator struct {
 	validator *validator.Validate
 }
 
+// Validate ...
 func (v *Validator) Validate(i interface{}) error {
 	return v.validator.Struct(i)
 }
